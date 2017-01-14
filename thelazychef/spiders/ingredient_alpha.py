090b6940031b25034b4d34719ec6723b51748703
khperez <khperez@uh.edu>
2017-01-14 00:30:40 -0600

    Recipe ingredient scraper in alpha

--- thelazychef/spiders/ingredient_alpha.py
+++ thelazychef/spiders/ingredient_alpha.py
@@ -1,13 +1,36 @@
 import scrapy
+import re
 
 class IngredientAlpha(scrapy.Spider):
     name = "ingredient_alpha"
     start_urls = [
-        "https://www.budgetbytes.com/2017/01/chicken-cranberry-salad-lemon-poppy-seed-dressing/"
+        "http://damndelicious.net/2016/10/18/ground-beef-noodle-stir-fry/"
     ]
 
     def parse(self, response):
+        measurements = ['lb', 'lbs', 'pound', 'pounds',
+                        'tbsp', 'tbsps', 'tablespoon', 'tablespoons',
+                        'tsp', 'teaspoon', 'teaspoons',
+                        'cup', 'cups',
+                        'oz', 'ounce', 'ounces']
         ingredients = response.xpath('//li[@class="ingredient"]/text()').extract()
-        with open("test.txt", 'wb') as f:
-            for ingredient in ingredients:
-                f.write("{}\n".format(ingredients))
+        target =  open("ingredients.txt", "wb")
+        for ingredient in ingredients:
+            ingredient_string = " "
+            ingredient = ingredient.encode('utf-8')
+            ingredient = ingredient.split(" ")
+            if ingredient[0].isdigit():
+                ingredient.remove(ingredient[0])
+            elif ingredient[0].find("/") is not -1:
+                ingredient.remove(ingredient[0])
+            elif re.match("[^\x00-\x7f]", ingredient[0]) is not None:
+                ingredient.remove(ingredient[0])
+            for word in ingredient:
+                # Find measurement labels
+                if word.lower() in measurements:
+                    ingredient.remove(word)
+                # Remove currency labels
+                if "$" in word:
+                    ingredient.remove(word)
+            ingredient_string = ingredient_string.join(ingredient)
+            target.write("{}\n".format(ingredient_string))
